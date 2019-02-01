774d88888727ddb39698d05add341175d581d5f9
Kabe.nr <adamkabena11@gmail.com>
2019-02-01 15:10:57 +0300

    Complete Variables Assignment

---
+++ main.py
@@ -0,0 +1,43 @@
+"""
+I CHOSE THE COMMENT BELOW AS THE TITLE FOR THIS ASSIGNMENT
+"""
+
+
+#HOMEWORK ASSIGNMENT#1: VARIABLES
+
+
+"""
+This assignment is based on the topic on variables.
+variables can be defined as containers.
+The can hold values of different data types such as integers,
+floats/doubles(decimals), and Strings. Variables can also hold
+boolean values; this are defined as true or false values
+"""
+
+Title = "You and Me" #This is a String, usually enclosed within quotation marks
+Genre = "Nu metal, Alternative metal, Hard Rock" #String
+Artist = "Memphis May Fire" #String
+Duration = 4.14 #The number on the left is a float, it represents the number of minutes and seconds the songs runs
+Studio = "The Hideout Studios" #String
+Producers = "Kane Churko, Kellen McGregor, Drew Fulk" #String
+Label = "Rise" #String
+Album = "Broken" #String
+TrackNo = 9 #The number on the left is an integer; it represents the track no of the song in the album
+ReleaseMonth = "November"
+ReleaseDate = 16 #integer
+ReleaseYear = 2018 #integer
+
+
+
+print("Title: ", Title)
+print("Genre: ", Genre)
+print("Artist: ", Artist)
+print("Duration: ", Duration)
+print("Studio: ", Studio)
+print("Producers: ", Producers)
+print("Label: ", Label)
+print("Album: ", Album)
+print("TrackNo: ", TrackNo)
+print("ReleaseMonth: ", ReleaseMonth)
+print("ReleaseDate: ", ReleaseDate)
+print("ReleaseYear: ", ReleaseYear)
