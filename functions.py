fa501f47957babd8171bbd4a8e990b416f50c9fd
Kabe.nr <adamkabena11@gmail.com>
2019-02-01 15:13:46 +0300

    Complete Functions Assignment

---
+++ functions.py
@@ -0,0 +1,33 @@
+#HOMEWORK ASSIGNMENT #2: FUNCTIONS
+
+"""
+Functions are pieces of code that can be used over and over through-out a program,
+of-course where necessary. :)
+"""
+
+
+def artistName():
+    print("Artist: Memphis May Fire")
+
+def genre():
+    print("Genre: Nu metal, Alternative Rock, Hard Rock")
+
+def releaseYear():
+    year = 2018
+    print("Year of release: ", year)
+
+artistName()
+genre()
+releaseYear()
+
+
+#For Extra credit
+
+x = 5
+y = 10
+
+i = 1
+j = 1
+print(bool(x==y))
+
+print(bool(i == j))
